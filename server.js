9c3beed4bf5fbeae9e940caff7e4652871e611b2
AnkammaChowdary <30653356+AnkammaChowdary@users.noreply.github.com>
2017-08-07 15:11:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/main.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.css'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 app.get('/Artical-one',function(req,res){
    res.sendFile(path.join(__dirname,'artical-one.html'));
