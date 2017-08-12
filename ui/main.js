fff7fa5e168d42b9922bb37847bf747b101684c4
AnkammaChowdary <30653356+AnkammaChowdary@users.noreply.github.com>
2017-08-12 22:14:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 //move img
 var img = document.getElementById('modi');
 img.onclick = function(){
-    img.style.marginLeft='++';
+    img.style.marginLeft='1555px';
 img.style.marginLeft='100px';
 };
