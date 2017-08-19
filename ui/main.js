013aa9b5dae28bbe17d1e7542aca994639046df9
AnkammaChowdary <30653356+AnkammaChowdary@users.noreply.github.com>
2017-08-19 23:03:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,9 @@
 //Counter Req
-var
+var button=document.getElementById('counter');
+ var counter=0;
+button.onclick= function(){
+  
+  counter=counter+1;
+  var spam=document.getElementById('count');
+  sapm.innerHTML=counter.toString();
+};
