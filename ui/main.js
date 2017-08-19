12c546e9c1c8f35a7d19e7f2a319da159731bafb
AnkammaChowdary <30653356+AnkammaChowdary@users.noreply.github.com>
2017-08-19 23:32:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,16 @@
 //Counter Req
 var button=document.getElementById('counter');
-var counter=0;
 button.onclick = function() {
-  
-  counter = counter+1;
-  var spam = document.getElementById('count');
-  spam.innerHTML = counter.toString();
+  var request = new XMLHttpRequest();
+  request.onreadystatechange= function(){
+      if(request.readyState=== XMLHttpRequest.DONE){
+          if(requeststatus===200){
+            var counter=request.responseText;
+            var spam = document.getElementById('count');
+            spam.innerHTML = counter.toString();
+          }
+      }
+  };
+  request.open('GET','http://u15pa1a1238.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
